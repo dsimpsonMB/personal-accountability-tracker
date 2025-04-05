1a9fad7a0e893c9f4537940e383b6e3982449e66
Daniel Simpson <dsimpson@missionbit.org>
2025-04-05 18:19:49 +0000

    add comments to completed code

--- code-along-complete/script.js
+++ code-along-complete/script.js
@@ -1,7 +1,65 @@
-const goals = []
+//First we need to get the data the user inputted into the form
+const goalForm = document.getElementById('goal-form')
+goalForm.addEventListener('submit', addGoal);
 
-const goalOutput = document.getElementById('goal-output')
+const goals = [] //where we'll store our goals
 
+function addGoal(e){
+  e.preventDefault()
+
+  //get values from input elements
+  const goalElement = document.getElementById('goal')
+  const goalValue = goalElement.value
+  
+  const deadlineElement = document.getElementById('deadline')
+  const deadlineValue = deadlineElement.value
+
+  const goalObject = {
+    goal: goalValue,
+    deadline: deadlineValue,
+    completed: false
+  }
+
+  goals.push(goalObject); //add those values (as a new goal object) to our array
+
+  updateGoalsOnPage() //update our ui to show all the goals in the array
+}
+
+//next create our function to update our ui
+function updateGoalsOnPage(){
+  const goalOutput = document.getElementById('goal-output') ////Select the area on the page that will hold our goals - "My Goals"
+  goalOutput.innerHTML = '' //clear goal list
+  goals.forEach(addGoalToPage) //loop through goals array and add each goal to the page
+}
+
+function addGoalToPage(goal,index){
+  const goalOutput = document.getElementById('goal-output') //Select the "My Goals" section of the page
+
+  const goalItem = createGoalItem(goal,index) //create the HTML element for the goal
+
+  goalOutput.appendChild(goalItem) //add that HTML element to the "My Goals" section
+}
+
+//create HTML list item element with all the child elements shown in the example
+function createGoalItem(goal, index){
+
+  const goalItem = document.createElement('li');
+  goalItem.innerHTML = `<strong>Goal:</strong> ${goal.goal} <br> <strong>Deadline:</strong> ${goal.deadline}`;
+
+  if(goal.completed){
+    goalItem.classList.add('crossed-off')
+  }
+  
+  const checkbox = createCheckbox(goal, index)
+  goalItem.prepend(checkbox)
+
+  const deleteButton = createDeleteButton(index)
+  goalItem.append(deleteButton)
+
+  return goalItem
+}
+
+//create checkbox to add to goal list item
 function createCheckbox(goal, index){
   const checkbox = document.createElement('input')
   checkbox.type = 'checkbox';
@@ -18,7 +76,8 @@ function createCheckbox(goal, index){
   return checkbox
 }
 
-function createDeleteButton(goal, index){
+//create delete button to add to goal list item
+function createDeleteButton(index){
   const deleteButton = document.createElement('button')
   deleteButton.textContent = 'Delete'
 
@@ -32,51 +91,8 @@ function createDeleteButton(goal, index){
   return deleteButton
 }
 
-function addGoalToPage(goal,index){
-  const goalItem = createGoalItem(goal,index)
 
-  goalOutput.appendChild(goalItem)
-}
 
-function createGoalItem(goal, index){
-  const goalItem = document.createElement('li');
-  goalItem.innerHTML = `<strong>Goal:</strong> ${goal.goal} <br> <strong>Deadline:</strong> ${goal.deadline}`;
 
-  if(goal.completed){
-    goalItem.classList.add('crossed-off')
-  }
-  
-  const checkbox = createCheckbox(goal, index)
-  goalItem.prepend(checkbox)
 
-  const deleteButton = createDeleteButton(goal, index)
-  goalItem.append(deleteButton)
 
-  return goalItem
-}
-
-function updateGoalsOnPage(){
-  goalOutput.innerHTML = '' //clear goal list
-  goals.forEach(addGoalToPage)
-}
-
-function addGoal(e){
-  e.preventDefault()
-
-  const goalElement = document.getElementById('goal')
-  const goalValue = goalElement.value
-  
-  const deadlineElement = document.getElementById('deadline')
-  const deadlineValue = deadlineElement.value
-
-  const goalObject = {
-    goal: goalValue,
-    deadline: deadlineValue,
-    completed: false
-  }
-
-  goals.push(goalObject);
-  updateGoalsOnPage()
-}
-
-document.getElementById('goal-form').addEventListener('submit', addGoal);
--- code-along-starter/script.js
+++ code-along-starter/script.js
@@ -1,18 +1,33 @@
-const goals = [] //where we'll store our goals
-
 //First we need to get the data the user inputted into the form
 const goalForm = document.getElementById('goal-form')
 
 goalForm.addEventListener('submit', addGoal);
 
-function addGoal(e){
+const goals = [] //where we'll store our goals
+
+//Next we want to add that goal to our goals array
+function addGoalToGoals(e){
   e.preventDefault()
+  //get values from input elements
+  
+  //add those values (as a new goal object) to our array
+  //update our ui to show all the goals in the array
+  updateGoalsOnPage();
 }
 
-//Next we need to add that goal to the page
-const goalOutput = document.getElementById('goal-output') //"My Goals" area
+//next create our function to update our ui
+function updateGoalsOnPage(){
+  //clear goal list
+  
+  //loop through goals array and add each goal to the page
+  goals.forEach(addGoalToPage)
+}
 
+//Adding each goal to the page as a ui element
 function addGoalToPage(goal,index){
+  //Select the area on the page that will hold our goals
+  const goalOutput = document.getElementById('goal-output') //"My Goals" area'
+
   //create HTML element for goal
   //add it to the page in the "My Goals Area"
 }
@@ -21,21 +36,18 @@ function createGoalItem(goal,index){
   //create HTML list item element with all the child elements shown in the example
 }
 
-function updateGoalsOnPage(){
-  //clear goal list
-  
-  //loop through goals array and add each goal to the page
-  goals.forEach(addGoalToPage)
-}
-
 //create checkbox to add to goal list item
 function createCheckbox(goal, index){
 }
 
 //create delete button to add to goal list item
-function createDeleteButton(goal, index){
+function createDeleteButton(index){
 
 }
+
+
+
+
 
 
 
